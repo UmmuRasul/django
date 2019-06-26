2475c886702a76aadcdae2e0aa262450113cd989
UmmuRasul <45812645+UmmuRasul@users.noreply.github.com>
2019-06-26 15:42:21 +0300

    modifiedMaster

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
 x = print("Am soo grareful to get this opportunity")
 def myapp():
   print(x)
+  
+  
 def change():
   print("the master of coding")
   
 change()
 myapp()
+
+y = {'name':'Mtu','age':5 }
 
